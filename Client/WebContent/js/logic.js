7b49cae619c90c89145e610cd76063c7897419cb
ArjyaSMishra <arjya.sm@gmail.com>
2017-01-04 04:11:25 +0100

    changes in GUI

--- Client/WebContent/js/logic.js
+++ Client/WebContent/js/logic.js
@@ -4,8 +4,9 @@
 
 var canvas = new fabric.Canvas('canvas');
 var canvas1 = new fabric.Canvas('canvas1');
-var sink_No = 0;
-var table_No = 0;
+var object_counter = 0;
+var x = 510;
+var y = 10;
 
 function drawGrid(){
 	var noOfBlocks = $("#arrayNumber").val();
@@ -51,37 +52,15 @@ function drawGrid(){
 	$("#grid_btn").disabled = false;
 	$("#arrayNumber").attr("disabled", "disabled");
 }
- 
-function addCircle() {
-    canvas1.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
-    canvas1.selectionColor = 'rgba(0,255,0,0.3)';
-    canvas1.selectionBorderColor = 'red';
-    canvas1.selectionLineWidth = 5;
+
+function addObject() {
+	var object= $("#objectSelect").val();
+	canvas1.add(new fabric.Circle({ radius: 10, fill: '#f55', left:x-510, top:y-10, id: object+"_"+ object_counter }));
+	object_counter++;
 }
 
-function addRect() {
-	var rect = new fabric.Rect({ width: 100, height: 50, fill: 'green' });
-	canvas1.add(rect);
-	rect.on('selected', function() {
-	  console.log('selected a rectangle');
-	}); 
-}
-	
-function addSink() {
-    canvas1.add(new fabric.Circle({ radius: 10, fill: '#f55', left: x-500, top: y, id: 'sink_' + sink_No })); 
-    sink_No++;
-}
-
-function addTable() {
-	canvas1.add(new fabric.Circle({ radius: 10, fill: '#000', left:x-500, top: y, id: 'table_' + table_No }));
-	table_No++;
-}
-
-function showImageTools (val) {
-    if (!$('#imageDialog').length) {
-    	$('body').append("<div id='imageDialog' style='position: absolute; top: y; left: x'><h1></h1></div>");
-    	$('h1').append(val);
-    }
+function showInfo(val) {
+	$('#messageDialog').text(val);
 }
 
 function deleteObject() {
@@ -102,43 +81,22 @@ canvas1.on('mouse:move',function(options){
     var pt = { x: options.e.clientX, y: options.e.clientY };
     
     if(options.target!= null) {
-    	/* if(options.target.radius == 10 && options.target.fill == "#f55"){
-    		showImageTools ("Sink");
-    	}
-    	if(options.target.radius == 10 && options.target.fill == "#000"){
-    		showImageTools ("Dining Table");
-    	} */
-    	
-    	showImageTools (options.target.id);
+    	showInfo(options.target.id);
     }  
 });
 
 canvas1.on('mouse:out', function(e) {
-     $('#imageDialog').remove();
+	$('#messageDialog').text("");
   });
   
 canvas1.on('object:added', function(e) {
     if(e.target!= null) {
-    	/* if(e.target.radius == 10 && e.target.fill == "#f55"){
-    		console.log("Sink created");
-    	}
-    	if(e.target.radius == 10 && e.target.fill == "#000"){
-    		console.log("Dining Table created");
-    	} */
-    	
     	console.log(e.target.id + " created");
     } 
  });
  
 canvas1.on('object:removed', function(e) {
     if(e.target!= null) {
-    	/* if(e.target.radius == 10 && e.target.fill == "#f55"){
-    		console.log("Sink deleted");
-    	}
-    	if(e.target.radius == 10 && e.target.fill == "#000"){
-    		console.log("Dining Table deleted");
-    	} */
-    	
     	console.log(e.target.id + " deleted");
     } 
  });
